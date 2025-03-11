1a1de82d6605c5c4c5781dcdaad4300ac95c8f3a
Sereja-Lebeda <lserg97@gmail.com>
2025-03-11 16:28:37 +0300

    First commit for test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sosi
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	// Получаем аргументы командной строки (первый - это сам исполняемый файл)
+	args := os.Args
+
+	// Если аргументов нет, выводим сообщение
+	if len(args) < 2 {
+		fmt.Println("Использование: program <параметры>")
+		return
+	}
+
+	// Выводим все переданные аргументы
+	fmt.Println("Переданные параметры:")
+	for i, arg := range args[1:] { // Пропускаем args[0] (имя исполняемого файла)
+		fmt.Printf("Аргумент %d: %s\n", i+1, arg)
+	}
+	time.Sleep(10 * time.Second)
+}
